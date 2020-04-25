110150e91c36d4a84ce09986a4477896ec5237cf
Tzian <psychotic.sarcasm@googlemail.com>
2020-04-25 14:55:17 +0100

    build script update

--- Assets/Cheky/Editor/BuildScript.cs
+++ Assets/Cheky/Editor/BuildScript.cs
@@ -19,8 +19,7 @@ namespace Cheky.Editor
 			{
 				locationPathName = "E:/_GameDev_LearningToMakeAGame/Cheky/OurGameName/ChekyGame.exe",
 				scenes = clientScenes,
-				target = BuildTarget.StandaloneWindows64,
-				options = BuildOptions.Development
+				target = BuildTarget.StandaloneWindows64
 			};
 
 			BuildPipeline.BuildPlayer(buildOptions);
@@ -41,8 +40,7 @@ namespace Cheky.Editor
 			{
 				locationPathName = "E:/_GameDev_LearningToMakeAGame/Cheky/OurGameName/Server/ChekyServer.exe",
 				scenes = serverScenes,
-				target = BuildTarget.StandaloneWindows64,
-				options = BuildOptions.Development
+				target = BuildTarget.StandaloneWindows64
 			};
 			BuildPipeline.BuildPlayer (buildOptions);
 		}
